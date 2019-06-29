9bc02d6d54fdb8dae4734d913b697327c1bf9509
Bermet Akynbekova <akynbekovabermet@gmail.com>
2019-06-29 15:30:53 +0600

    Добавлено приложение accounts и функция входа для пользователей

--- library/library/urls.py
+++ library/library/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include(('django.contrib.auth.urls', 'accounts'), namespace='accounts')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('', include('webapp.urls', namespace='webapp'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
