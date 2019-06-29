a118965d567055d3aa9b262d3b525b6897f42df7
Bermet Akynbekova <akynbekovabermet@gmail.com>
2019-06-29 15:09:45 +0600

    Добавлена модель Книги

--- library/webapp/admin.py
+++ library/webapp/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Author
+from .models import Author, Book
 
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['pk', 'name']
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'title']
+
 
 admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
--- library/webapp/models.py
+++ library/webapp/models.py
@@ -17,7 +17,7 @@ class Author(models.Model):
     birth_date = models.DateField(blank=True, null=True)
     death_date = models.DateField(blank=True, null=True)
     bio = models.TextField(max_length=5000, blank=True, null=True)
-    photo = models.ImageField(upload_to='uploads/photos', blank=True, null=True)
+    photo = models.ImageField(upload_to='photos', blank=True, null=True)
     is_deleted = models.BooleanField(default=False)
 
     objects = SoftDeleteManager()
@@ -34,3 +34,23 @@ class Author(models.Model):
 
 
 
+class Book(models.Model):
+    title = models.CharField(max_length=255, blank=False, null=False)
+    author = models.ForeignKey(Author, blank=False, null=False, on_delete=models.CASCADE, related_name='books')
+    published = models.CharField(max_length=4, null=False, blank=False)
+    file = models.FileField(upload_to='books', blank=True, null=True)
+    cover = models.ImageField(upload_to='covers', blank=True, null=True)
+    description = models.TextField(max_length=5000, blank=True, null=True)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteManager()
+
+    def __str__(self):
+        return f"{self.pk}, {self.title}"
+
+    def get_absolute_url(self):
+        return reverse('webapp:book_detail', kwargs={'pk': self.pk})
+
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
